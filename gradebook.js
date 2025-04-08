ec97f8c196746ced150d762ce95a45d11be686b0
Arickson Arnold <86798534+Arickson@users.noreply.github.com>
2025-04-08 13:22:28 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+//Todo: Getch data from the PostgreSQL database 
+function fetchGradeData() {
+    //This function will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...")
+}
+
+//Todo: Populate the table with grade data
+function populateGradebook(data) {
+    //This function will take the fetched grade data and populate the table
+    console.log("Populating gradebook with data:", data)
+}
+
+//Todo: Remove this
+//Call the stubs to demostrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//End Remove
